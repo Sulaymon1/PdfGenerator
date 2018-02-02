aba5e9dc648cb49cd7d417cc74e9aa11082e0bdd
Sulaymon <demirel6777@gmail.com>
2018-02-02 22:00:44 +0300

    Fixed App.java

--- src/main/java/ru/itis/PdfGenerator/PdfGeneratorApplication.java
+++ src/main/java/ru/itis/PdfGenerator/PdfGeneratorApplication.java
@@ -1,9 +1,16 @@
 package ru.itis.PdfGenerator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ru.itis.repositories")
+@ComponentScan(basePackages = "ru.itis")
+@EntityScan(basePackages = "ru.itis")
 public class PdfGeneratorApplication {
 
 	public static void main(String[] args) {
